846894273ee36b06fdbc93a60d5ef467a9acfdf9
Mustafa <mustafa_khanani@outlook.com>
2024-02-23 15:37:34 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+var cacheName ='petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/Yarn.jpg',
+    'images/Cat Bed.jpg',
+    'images/Cat Food.jpg',
+    'images/Cat Scratching Post.jpg',
+    'images/Kitty Litter.jpg',
+    'images/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        //check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            // 'r' is the matching file if it exists in the cache
+            return r
+        })
+    );
+});
